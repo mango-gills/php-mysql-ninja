9c9a458963a41f47de61baa0a52831375ac564f9
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-04-22 16:17:19 +0800

    update css, header title

--- src/index.php
+++ src/index.php
@@ -2,13 +2,27 @@
 
 include('header.php');
 
+$blogs = [
+    ['mario party', 'mario', 'lorem', 30],
+    ['mario kart', 'toad', 'lorem', 25],
+    ['zelda hidden chest', 'link', 'lorem', 50]
+];
+
+$blogs2 = [
+    ['title' => 'mario party', 'author' => 'mario', 'content' => 'lorem', 'likes' => 30],
+    ['title' => 'mario kart', 'author' => 'toad', 'content' => 'lorem', 'likes' => 25],
+    ['title' => 'zeldas hidden chest', 'author' => 'link', 'content' => 'lorem', 'likes' => 50],
+
+];
+
+$blogs2[] = ['title' => 'castle party', 'author' => 'peach', 'content' => 'lorem', 'likes' => 100];
 
 ?>
 
 <body class="container mx-auto pt-5">
-    <div class="text-blue-500 text-2xl font-bold ">
+    <div class="">
         <?php
-        echo "This is the initial commit";
+        echo '<h1 class="text-red-500 text-2xl font-bold">Hello World!</h1>'
         ?>
     </div>
 
