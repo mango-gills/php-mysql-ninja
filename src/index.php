e294172906f4b75a97e29548be7dc7487daacb5c
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-04-22 16:18:48 +0800

    remove test variables

--- src/index.php
+++ src/index.php
@@ -2,21 +2,6 @@
 
 include('header.php');
 
-$blogs = [
-    ['mario party', 'mario', 'lorem', 30],
-    ['mario kart', 'toad', 'lorem', 25],
-    ['zelda hidden chest', 'link', 'lorem', 50]
-];
-
-$blogs2 = [
-    ['title' => 'mario party', 'author' => 'mario', 'content' => 'lorem', 'likes' => 30],
-    ['title' => 'mario kart', 'author' => 'toad', 'content' => 'lorem', 'likes' => 25],
-    ['title' => 'zeldas hidden chest', 'author' => 'link', 'content' => 'lorem', 'likes' => 50],
-
-];
-
-$blogs2[] = ['title' => 'castle party', 'author' => 'peach', 'content' => 'lorem', 'likes' => 100];
-
 ?>
 
 <body class="container mx-auto pt-5">
