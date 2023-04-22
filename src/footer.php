c56cb4e01f1de757b12dfb0c5dae9e1c62aec0c6
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-04-22 14:58:34 +0800

    Initial commit

---
+++ src/footer.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+    <div>Copyright 2023</div>
+</footer>
+
+</html>
---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+
+include('header.php');
+
+
+?>
+
+<body class="container mx-auto pt-5">
+    <div class="text-blue-500 text-2xl font-bold ">
+        <?php
+        echo "This is the initial commit";
+        ?>
+    </div>
+
+</body>
+<?php
+
+include('footer.php')
+?>
